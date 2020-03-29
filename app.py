37591256390f09744d7499e494baf96240b4c343
Alex kagai <alexkagai@outlook.com>
2020-03-29 19:41:51 +0300

    Finishes tasting 2(example 2)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+import random
+import time
 
 from flask import Flask, request, render_template, session, \
-    flash, redirect, url_for
+    flash, redirect, url_for, jsonify
 from flask_mail import Mail, Message
 from celery import Celery
 
@@ -31,6 +33,29 @@ def send_async_email(email_data):
         mail.send(msg)
 
 
+@celery.task(bind=True)
+def long_task(self):
+    """Background task that runs a long 
+        function with progress reports.
+    """
+
+    verb = ['Reading', 'Retrieving', 'Preparing', 'Mixing', 'Tasting']
+    adjective = ['nutritious', 'heavy', 'lean', 'quick', 'neat']
+    noun = ['sandwich', 'cupcakes', 'cookies', 'nuts mix', 'biscuits']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(verb),
+                                              random.choice(adjective),
+                                              random.choice(noun))
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
+
 ################# Celery tripper(app route) ###############
 
 @app.route('/', methods=['GET', 'POST'])
@@ -58,6 +83,46 @@ def index():
         flash('An email will be sent to {0} in one minute'.format(email))
 
     return redirect(url_for('index'))
+
+
+@app.route('/longtask', methods=['POST'])
+def longtask():
+    task = long_task.apply_async()
+    return jsonify({}), 202, {'Location': url_for('taskstatus',task_id=task.id)}
+
+
+def res_data(state, current, total, status):
+    """Build response data for a task
+    """
+    return {
+            'state': state,
+            'current': current,
+            'total': total,
+            'status': status,
+        }
+
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = res_data(task.state, 0, 1, 'Pending...')
+    
+    elif task.state != 'FAILURE':
+        response = res_data(task.state, task.info.get('current', 0), 
+                            task.info.get('total', 1),
+                            task.info.get('status', '')
+                    )
+        
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = res_data(task.state, 1, 1, str(task.info))
+        # this is the exception raised
+        
+    return jsonify(response)
 
 
 if __name__ == '__main__':
